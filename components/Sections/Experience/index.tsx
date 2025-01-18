80f6babd147de212e25601ddbb65030455cc7b3a
jonylazarte <85182911+jonylazarte@users.noreply.github.com>
2025-01-18 20:53:42 -0300

    Delete components directory

--- components/Sections/Experience/index.tsx
+++
@@ -1,8 +0,0 @@
-import { memo } from 'react'
-import { Heading, Text, Stack, Link } from '@chakra-ui/react'
-import ExperienceTab from './ExperienceTab'
-const DetailSection = () => (
-  <></>
-)
-
-export default memo(DetailSection)
