4be58ae1b179ecf660d658351a937192ff9b2114
jonylazarte <85182911+jonylazarte@users.noreply.github.com>
2025-01-18 21:07:03 -0300

    Add files via upload

---
+++ components/Sections/Experience/index.tsx
@@ -0,0 +1,8 @@
+import { memo } from 'react'
+import { Heading, Text, Stack, Link } from '@chakra-ui/react'
+import ExperienceTab from './ExperienceTab'
+const DetailSection = () => (
+  <></>
+)
+
+export default memo(DetailSection)
